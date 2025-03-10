1bd7012a0b2797f9a7c748e1bea6c5221f9f3106
Nicolas Fella <nicolas.fella@gmx.de>
2025-03-10 13:43:41 +0100

    More stuff

---
+++ BlockLibMacros.cmake
@@ -0,0 +1,70 @@
+# define global block building command
+
+function(gr_generate_block_instantiations LIB_NAME)
+    set(options "")
+    set(oneValueArgs "")
+    set(multiValueArgs HEADERS SOURCES)
+    cmake_parse_arguments(CBL "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    if (NOT TARGET ${LIB_NAME})
+        message(FATAL_ERROR "${LIB_NAME} is not a target")
+    endif()
+
+    if (NOT CBL_HEADERS)
+        message(FATAL_ERROR "No headers passed to gr_generate_block_instantiations")
+    endif()
+
+    # directory where parse_registrations will output .cpp files.
+    set(GEN_DIR "${CMAKE_BINARY_DIR}/generated_plugins/${LIB_NAME}")
+    file(MAKE_DIRECTORY "${GEN_DIR}")
+
+    set(GEN_CPP_LIST "")
+    foreach(HDR IN LISTS CBL_HEADERS)
+        get_filename_component(ABS_HDR "${HDR}" ABSOLUTE)
+        get_filename_component(BASENAME "${HDR}" NAME_WE)
+
+        # remove any previously generated .cpp files for this header.
+        file(GLOB OLD_FILES "${GEN_DIR}/${BASENAME}*.cpp")
+        if(OLD_FILES)
+            message(STATUS "deleting old generated files ${OLD_FILES}")
+            file(REMOVE ${OLD_FILES})
+        endif()
+
+        if(ENABLE_SPLIT)
+            set(PARSER_SPLIT_OPTION "--split")
+        else()
+            set(PARSER_SPLIT_OPTION "")
+        endif()
+
+        message(STATUS "Generating code for header: ${ABS_HDR} -> ${GEN_DIR} with PARSER_SPLIT_OPTION=${PARSER_SPLIT_OPTION}")
+
+        # run the parser tool immediately at configuration time.
+        execute_process(
+                COMMAND ${PARSER_EXECUTABLE} "${ABS_HDR}" "${GEN_DIR}" ${PARSER_SPLIT_OPTION}
+                RESULT_VARIABLE gen_res
+                OUTPUT_VARIABLE gen_out
+                ERROR_VARIABLE gen_err
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+                ERROR_STRIP_TRAILING_WHITESPACE
+        )
+        message(STATUS "Output from parse_registrations for ${ABS_HDR}:\n${gen_out}")
+        if(NOT gen_res EQUAL 0)
+            message(FATAL_ERROR "Error running parse_registrations on ${HDR}: ${gen_err}")
+        endif()
+
+        # Glob for the generated .cpp files.
+        file(GLOB GENERATED_FILES "${GEN_DIR}/${BASENAME}*.cpp")
+        if(NOT GENERATED_FILES)
+            # If no .cpp files were generated, create a dummy file.
+            set(DUMMY_CPP "${GEN_DIR}/dummy_${BASENAME}.cpp")
+            file(WRITE "${DUMMY_CPP}" "// No macros or expansions found for '${BASENAME}'\n")
+            list(APPEND GENERATED_FILES "${DUMMY_CPP}")
+        endif()
+
+        list(APPEND GEN_CPP_LIST ${GENERATED_FILES})
+    endforeach()
+
+    target_sources(${LIB_NAME} PRIVATE ${GEN_CPP_LIST})
+
+    target_link_libraries(${LIB_NAME} PRIVATE BlockLib::Registry)
+endfunction()
